1f515137fdb733f9859352c49e745874ca1e19d8
David Horvath <dhorvath@c3r1p7.hive.fi>
2023-12-17 16:33:53 +0200

    errorhandling for dup2 added

--- src/commands.c
+++ src/commands.c
@@ -6,12 +6,13 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 16:22:27 by dhorvath          #+#    #+#             */
-/*   Updated: 2023/12/16 16:53:12 by dhorvath         ###   ########.fr       */
+/*   Updated: 2023/12/17 15:18:16 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include "ft_printf.h"
+#include "libft.h"
 
 void	wait_for_commands(pid_t *pids)
 {
@@ -21,17 +22,23 @@ void	wait_for_commands(pid_t *pids)
 
 	i = 0;
 	exit_status = 0;
-	while (pids[i])
+	while (pids && pids[i])
 	{
 		waitpid(pids[i], &status, 0);
 		if (WIFEXITED(status))
 			exit_status = WEXITSTATUS(status);
-		if (WIFSIGNALED(status))
-			exit_status = WTERMSIG(status);
+		if (exit_status == -1)
+			break ;
 		i++;
 	}
 	free(pids);
 	exit(exit_status);
+}
+
+int	child_error(void)
+{
+	print_error(0);
+	exit(-1);
 }
 
 void	call_command(int fds[2], t_command cmd, int *pids, int i)
@@ -45,20 +52,20 @@ void	call_command(int fds[2], t_command cmd, int *pids, int i)
 		path = find_command(cmd.params, cmd.env);
 		if (!path)
 		{
-			ft_printf("command not found: %s\n", cmd.params[0]);
+			ft_putstr_fd("pipex: command not found: ", 2);
+			ft_putendl_fd(cmd.params[0], 2);
 			exit(127);
 		}
-		dup2(fds[0], 0);
-		dup2(fds[1], 1);
+		if (dup2(fds[0], 0) == -1 || dup2(fds[1], 1) == -1)
+			child_error();
 		execve(path, cmd.params, cmd.env);
 	}
 	else if (pid > 0)
 	{
 		if (!pids)
-		{
-			print_error(0);
-			exit(0);
-		}
+			child_error();
 		pids[i] = pid;
 	}
+	else
+		child_error();
 }
