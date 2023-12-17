28a1efabd4e3e3d6ef90ed2243e285a9c3914500
David Horvath <dhorvath@c3r1p7.hive.fi>
2023-12-17 16:37:59 +0200

    wrong filedescriptor issue for bonus fixed

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:12:48 by dhorvath          #+#    #+#             */
-/*   Updated: 2023/12/16 17:15:55 by dhorvath         ###   ########.fr       */
+/*   Updated: 2023/12/17 16:37:24 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,24 @@ int	main(int argc, char **argv, char **env)
 	int			fds[2];
 	int			prev_out;
 	pid_t		*pids;
-	t_command	cmd;
+	char		**params;
 
 	if (argc < 4)
 		return (print_usage());
 	pids = ft_calloc(argc - 2, sizeof(pid_t));
 	prev_out = 0;
-	cmd.env = env;
 	i = 0;
 	while (i < argc - 3)
 	{
-		cmd.params = ft_split(argv[i + 2], ' ');
-		if (!cmd.params)
+		params = ft_split(argv[i + 2], ' ');
+		if (!params)
 			return (print_error(0));
 		if (get_fds(i, (t_args){argc, argv}, fds, &prev_out) == 1)
-			return (print_error(cmd.params));
-		call_command(fds, cmd, pids, i++);
+			return (print_error(params));
+		else
+			call_command(fds, (t_command){params, env, 0}, pids, i++);
 		close(fds[1]);
-		free_args(cmd.params);
+		free_args(params);
 	}
 	close(prev_out);
 	wait_for_commands(pids);
